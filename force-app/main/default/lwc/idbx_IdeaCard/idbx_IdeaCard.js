d3afe61a1a9ffede7ceb2c5783d0070a79b15b10
KyleHanz.Andaya <KyleHanz.Andaya@sophos.com>
2025-09-30 00:12:40 +0800

    ITASINTPRJ-681 - Initial UI Layout

---
+++ force-app/main/default/lwc/idbx_IdeaCard/idbx_IdeaCard.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Idbx_IdeaCard extends LightningElement {}
---
+++ force-app/main/default/lwc/idbx_IdeasPage/idbx_IdeasPage.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class Idbx_IdeasPage extends LightningElement {
+
+}
