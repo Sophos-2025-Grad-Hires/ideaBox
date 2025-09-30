84ebeb1be7c0778756b075b8f9c2d6f38ffa27c9
KyleHanz.Andaya <KyleHanz.Andaya@sophos.com>
2025-09-30 15:40:52 +0800

    ITASINTPRJ-681 - Update Idea names to link to record pages

--- force-app/main/default/lwc/idbx_IdeaCard/idbx_IdeaCard.js
+++ force-app/main/default/lwc/idbx_IdeaCard/idbx_IdeaCard.js
@@ -1,5 +1,20 @@
 import { api, LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class Idbx_IdeaCard extends LightningElement {
+export default class Idbx_IdeaCard extends NavigationMixin(LightningElement) {
    @api idea;
+   ideaPageURL = 'https://google.com';
+
+   connectedCallback() {
+        // Generate a URL to the record page
+        this[NavigationMixin.GenerateUrl]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.idea.Id,
+                actionName: 'view',
+            },
+        }).then(url => {
+            this.ideaPageURL = url;
+        });
+    }
 }
