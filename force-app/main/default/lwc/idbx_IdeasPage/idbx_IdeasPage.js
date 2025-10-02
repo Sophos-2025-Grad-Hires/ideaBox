88ac834a93f659ac2836c79699de2fc977ef0dfb
KyleHanz.Andaya <KyleHanz.Andaya@sophos.com>
2025-10-02 21:33:52 +0800

    ITASINTPRJ-725 - Implement Ideas Page Search Bar functionality

--- force-app/main/default/lwc/idbx_IdeasPage/idbx_IdeasPage.js
+++ force-app/main/default/lwc/idbx_IdeasPage/idbx_IdeasPage.js
@@ -2,21 +2,48 @@ import { wire, LightningElement } from 'lwc';
 import selectAll from '@salesforce/apex/IdeaSelector.selectAll';
 
 export default class Idbx_IdeasPage extends LightningElement {
-    ideas;
+    ideasUnfiltered;
     error;
     
     @wire(selectAll)
     wiredIdeas({ error, data }) {
         if (data) {
-            this.ideas = data;
+            this.ideasUnfiltered = data;
             this.error = undefined;
         } else if (error) {
             this.error = error;
-            this.ideas = undefined;
+            this.ideasUnfiltered = undefined;
         }
     }
 
     get noIdeasAvailable() {
-        return !this.ideas || this.ideas.length === 0;
+        return !this.ideasUnfiltered || this.ideasUnfiltered.length === 0;
+    }
+
+    get ideas() {
+        if (this.queryTerm && this.queryTerm.trim() !== '') {
+            const lowerCaseQuery = this.queryTerm.toLowerCase();
+            return this.ideasUnfiltered.filter(idea =>
+                idea.Name.toLowerCase().includes(lowerCaseQuery)
+            );
+        }
+        return this.ideasUnfiltered;
+    }
+
+    // Search functionality
+    queryTerm;
+
+    handleSearch(event) {
+        const isEnterKey = event.keyCode === 13;
+
+        if (isEnterKey) {
+            this.queryTerm = event.target.value;
+        }
+    }
+
+    handleClearSearchBar(event) {
+        if (event.target.value === '') {
+            this.queryTerm = '';
+        }
     }
 }
